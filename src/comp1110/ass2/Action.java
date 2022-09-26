dcfb00b4056bf2fc3852350dac48e874293fec94
John Larkin <u6717250@anu.edu.au>
2022-09-27 00:08:44 +1000

    Action class

---
+++ src/comp1110/ass2/Action.java
@@ -0,0 +1,30 @@
+package comp1110.ass2;
+import comp1110.ass2.CatanDice;
+
+public class Action {
+    String type;
+    char structure;
+    int id;
+    int in;
+    int out;
+
+    // Assumes the action is well-formed
+    Action(String action_string){
+        char first = action_string.charAt(0);
+        if (first == 'b'){
+            this.type = "build";
+            this.structure = action_string.charAt(6);
+            this.id = Integer.parseInt(action_string.substring(7));
+        }
+        if (first == 't'){
+            this.type = "trade";
+            this.in = Integer.parseInt(action_string.substring(6));
+        }
+        if (first == 's'){
+            this.type = "swap";
+            this.in = Integer.parseInt(action_string.substring(7));
+            this.out = Integer.parseInt(action_string.substring(5,6));
+        }
+    }
+
+}
---
+++ tests/comp1110/ass2/TestAction.java
@@ -0,0 +1,31 @@
+package comp1110.ass2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+@Timeout(value = 2000, unit = TimeUnit.MILLISECONDS)
+public class TestAction {
+
+    // Create an action
+    public Action makeAction(String action_string){
+        return new Action(action_string);
+    }
+
+    @ Test
+    public void testConstructor(){
+        Action action = new Action("build J1");
+        Assertions.assertEquals(1, action.id, "Expected an id of 1 but got an id of " + action.id);
+        Assertions.assertEquals('J', action.structure, "Expected a character of J but got a character of " + action.structure);
+        Assertions.assertEquals("build", action.type);
+        Action action1 = new Action("trade 3");
+        Assertions.assertEquals("trade", action1.type);
+        Assertions.assertEquals(3, action1.in);
+        Action action2 = new Action("swap 3 2");
+        Assertions.assertEquals("swap", action2.type);
+        Assertions.assertEquals(2, action2.in);
+        Assertions.assertEquals(3, action2.out);
+    }
+}
