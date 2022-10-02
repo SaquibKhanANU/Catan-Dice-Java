21f9bd11cd34560753f16f4d1eb76f0385fd2daf
John Larkin <u6717250@anu.edu.au>
2022-10-02 12:03:05 +1100

    CatanPlayer and GameState

--- src/comp1110/ass2/CatanGame/CatanPlayer.java
+++ src/comp1110/ass2/CatanGame/CatanPlayer.java
@@ -1,4 +1,37 @@
 package comp1110.ass2.CatanGame;
 
+import comp1110.ass2.CatanStructure.Structure;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CatanPlayer {
+
+    // The index distinguishes the player, either 0,1,2,3
+    // The choice of 0,1,2,3 is so that in an array CatanPlayer[] players = new CatanPlayer[number of players].
+    // The ith player can be referenced with players[i].
+    // Then players[i].index = i.
+    int index;
+    // The board state of the current player, an array of structures.
+    // structures[0].id will return the id of position 0, for example R1.
+    ArrayList<Structure> structures;
+    // The current resources available
+    int[] resource_state;
+    // The current score of the player
+    int score;
+    // The current number of completed turns of the player
+    int turn_num;
+
+
+    public CatanPlayer(int index){
+        // Initialise the values associated to this player
+        this.index = index;
+        this.structures = new ArrayList<>();
+        this.resource_state = new int[6];
+        this.score = 0;
+        this.turn_num = 0;
+    }
+
+
+
 }
---
+++ src/comp1110/ass2/CatanGame/GameState.java
@@ -0,0 +1,42 @@
+package comp1110.ass2.CatanGame;
+
+
+// This class contains all the information for a game of CatanDice.
+// When a GameState is constructed the number of players in the game is
+// specified
+
+/**
+ * This class contains information to specify a game of Catan Dice.
+ * When a GameState is constructed the number of players needs to be specified.
+ * Then it creates an array of CatanPlayer objects which each specify a player
+ * in the game. The GameState also keeps track of the round number,
+ * turn number (between 0 and num-1 inclusive).
+ */
+
+public class GameState {
+
+    // The number of players, between 2 and 4.
+    int num;
+
+    // The index of the current players turn
+    int turn_num;
+
+    // The number of completed rounds (for example if each player
+    // has had one turn then it is has been one round.
+    int round;
+
+    public void GameState(int num){
+        // Initialise the values
+        this.num = num;
+        this.turn_num = 0;
+        this.round = 0;
+
+        // Use the CatanPlayer constructor to construct num players.
+        // Each player can be references using players[i]
+        CatanPlayer[] players = new CatanPlayer[num];
+        for (int i = 0; i < players.length; i ++){
+            players[i] = new CatanPlayer(i);
+        }
+
+    };
+}
