d6d0e049f20e053b97fafe48cce973a000d6e06b
John Larkin <u6717250@anu.edu.au>
2022-10-20 16:54:19 +1100

    instructions

--- src/comp1110/ass2/gui/Scenes/Instructions.java
+++ src/comp1110/ass2/gui/Scenes/Instructions.java
@@ -15,47 +15,79 @@ import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
-// TODO: John: Finish this class to give instructions when selected (refer to Winner.java)
+// Author: John Larkin and Saquib Khan
 public class Instructions extends Pane {
 
-//    Group board = new Group();
-//    public Instructions() {
-//        goBack();
-//        this.setBackground(new Background(new BackgroundFill(Color.web("#439527"), CornerRadii.EMPTY, Insets.EMPTY)));
-//        getChildren().addAll(new Instructions.InstructionsBoard(), board);
-//    }
-//
-//    // Author:
-//    class InstructionsBoard extends Rectangle {
-//        public InstructionsBoard() {
-//            setHeight(120);
-//            setWidth(300);
-//            setX(450);
-//            setY(150);
-//            setFill(Color.TAN);
-//            setStroke(Color.SADDLEBROWN);
-//            setStrokeWidth(3);
-//
-//                Text swap = new Text();
-//                swap.setText("catanPlayer.name + ");
-//                swap.setX(460);
-//                swap.setY(140);
-//                swap.setFill(Color.DARKGREEN);
-//                swap.setFont(Font.font("times new roman", FontWeight.BOLD, FontPosture.REGULAR, 30));
-//                board.getChildren().add(swap);
-//
-//        }
-//    }
-//
-//    // TOD0: FIX THIS
-//    private void goBack() {
-//        Button goBack = new Button();
-//        goBack.setText("GO BACK");
-//        goBack.setLayoutX(100);
-//        goBack.setLayoutY(500);
-//        goBack.setOnMousePressed(e -> {
-//            Game.scenes.activate("MENU");
-//        });
-//        board.getChildren().add(goBack);
-//    }
+    static String dice_roll_instruction = "some, text this is a placeholder for the text \n this is a test to see if it works";
+
+    Group board = new Group();
+    public Instructions() {
+        goBack();
+        InstructionsText instruction1 = new InstructionsText(dice_roll_instruction, "Dice Roll", 30, 60);
+        InstructionsText instruction2 = new InstructionsText(dice_roll_instruction, "Dice Roll", 30+550+20, 60);
+        InstructionsText instruction3 = new InstructionsText(dice_roll_instruction, "Dice Roll", 30, 60+350);
+        InstructionsText instruction4 = new InstructionsText(dice_roll_instruction, "Dice Roll", 30+550+20, 60+350);
+        this.setBackground(new Background(new BackgroundFill(Color.web("#439527"), CornerRadii.EMPTY, Insets.EMPTY)));
+        getChildren().addAll(board, instruction1, instruction2, instruction3, instruction4);
+    }
+
+    // Author:
+    class InstructionsBoard extends Rectangle {
+        public InstructionsBoard(double x, double y) {
+            setHeight(300);
+            setWidth(550);
+            setX(x);
+            setY(y);
+            setFill(Color.TAN);
+            setStroke(Color.SADDLEBROWN);
+            setStrokeWidth(3);
+
+
+        }
+    }
+
+    // TOD0: FIX THIS
+    private void goBack() {
+        Button goBack = new Button();
+        goBack.setText("GO BACK");
+        goBack.setLayoutX(1200-68);
+        goBack.setLayoutY(700-35);
+        goBack.setOnMousePressed(e -> {
+            Game.scenes.activate("Menu");
+        });
+        board.getChildren().add(goBack);
+    }
+
+    class InstructionsText extends Text{
+
+        InstructionsText(String Instructions, String title, double x, double y){
+
+
+            Text title1 = new Text();
+            title1.setText(title);
+            title1.setX(x);
+            title1.setY(y-25);
+            title1.setFill(Color.SADDLEBROWN);
+            title1.setFont(Font.font("times new roman", FontWeight.BOLD, FontPosture.REGULAR, 35));
+
+
+            setText(Instructions);
+            setX(x+10);
+            setY(y+10);
+            setFill(Color.DARKGREEN);
+            setFont(Font.font("times new roman", FontWeight.BOLD, FontPosture.REGULAR, 20));
+
+
+            InstructionsBoard square = new InstructionsBoard(x-20, y-20);
+
+
+
+            board.getChildren().addAll(this,square,title1);
+
+
+        }
+
+
+    }
+
 }
